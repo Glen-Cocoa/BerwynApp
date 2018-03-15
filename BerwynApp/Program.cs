22e63d60e64879003480f6372bd48530be55f23a
glen-cocoa <eli.steiger@gmail.com>
2018-03-15 11:46:01 -0400

    figured out how to access the csv file within the console application, using streamreader to build and write a single list that is the entire file currently.

--- BerwynApp/Program.cs
+++ BerwynApp/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace BerwynApp
@@ -10,6 +11,18 @@ namespace BerwynApp
     {
         static void Main(string[] args)
         {
+            using (var reader = new StreamReader(@"../../csv/test.csv"))
+            {
+                List<string> listA = new List<string>();
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+
+                    listA.Add(line);
+                    Console.Write(line);
+                }
+            }
         }
     }
 }
