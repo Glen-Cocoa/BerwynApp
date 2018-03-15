a41dc033fcd7df665b25e3f659202efa774a4421
glen-cocoa <eli.steiger@gmail.com>
2018-03-15 12:49:49 -0400

    pulling in csv and assigning values to corresponding lists

--- BerwynApp/Program.cs
+++ BerwynApp/Program.cs
@@ -13,18 +13,29 @@ namespace BerwynApp
         {
             using (var reader = new StreamReader(@"../../csv/test.csv"))
             {
+                //count keeps track of current row and also total rows
+                int count = 0;
+
+                //Lists to keep track of values after being parsed
                 List<string> GUID = new List<string>();
                 List<string> Val1 = new List<string>();
                 List<string> Val2 = new List<string>();
                 List<string> Val3 = new List<string>();
-                int count = 0;
 
+                //Loops through entire test.csv and assigns each value to its corresponding list
                 while (!reader.EndOfStream)
                 {
-                    var line = reader.ReadLine();
-                    GUID.Add(line);
+                    var data = reader.ReadLine();
                     count++;
-                    Console.WriteLine(count);
+                    string[] values = data.Split(',');
+                    string GUIDVal = values[0];
+                    string Val1Val = values[1];
+                    string Val2Val = values[2];
+                    string Val3Val = values[3];
+                    GUID.Add(GUIDVal);
+                    Val1.Add(Val1Val);
+                    Val2.Add(Val2Val);
+                    Val3.Add(Val3Val);
                 }
             }
         }
